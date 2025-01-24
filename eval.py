b2b4c416db1de5b9fb13a347c40b69aee1a91e7c
gdac <gabrieldalcs@gmail.com>
2025-01-24 17:06:59 -0300

    solve avaliation problem

--- eval.py
+++ eval.py
@@ -1,193 +1,17 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+# from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
+# from sklearn.preprocessing import OrdinalEncoder
+# import numpy as np
+# from fuzzywuzzy import fuzz
 from tqdm import tqdm
 from langchain_ollama import OllamaLLM
 import pandas as pd
 import json
-from sklearn.metrics import accuracy_score
-from fuzzywuzzy import fuzz
 import os
 
 def save_test_shuffled(test_set):
     test_json = test_set.to_json(orient="records", indent=4)
     with open("eval-data/test_recente.json", "w") as f:
         f.write(test_json)
-
-
-def req(input_df, new_test_set, model): 
-    columns = ["intent", "category", "action", "requirement", "targets", "magnitude", "start_time", "end_time"]
-    output_df = pd.DataFrame(columns=columns)
-    falhas = []
-    json_to_save = []
-    i = 0
-    for _, row  in tqdm(input_df.iterrows(), desc="Obtendo outputs"):
-        llm = OllamaLLM(model=model)
-        intent = row['intent']
-        response = llm.invoke(intent)
-        if response:
-            response = response.strip()
-            #print(response)
-            try:
-                response = json.loads(response)
-            except json.JSONDecodeError as e:
-                new_test_set = new_test_set.drop(index=i).reset_index(drop=True)
-                falhas.append((response, i, e))
-                #output_df = output_df.drop(index=1)
-                continue
-            json_to_save.append(response)
-            new_row = pd.DataFrame([response])
-            output_df = pd.concat([output_df, new_row], ignore_index=True)
-        else:
-            print("Nao respondeu nada")
-        
-        #print(new_test_set)
-        
-        
-        i += 1
-
-
-    output_df = output_df.drop(columns=['start_time', 'end_time'])
-    output_df = output_df.fillna("")
-    output_df = output_df.astype(str)
-    output_df = output_df.applymap(lambda x: x.lower())
-    with open('eval-data/output_recente.json', 'w') as f:
-        json.dump(json_to_save, f, indent=4)  
-
-   
-  
-    return output_df, new_test_set      
-
-
-
-def metrics_by_category(test, output):
-    # Combinar os DataFrames test e output para comparação
-    merged = test[['category', 'action']].merge(output[['category', 'action']], 
-                                                left_index=True, 
-                                                right_index=True, 
-                                                suffixes=('_true', '_pred'))
-
-    # Filtrar categorias únicas
-    categories = merged['category_true'].unique()
-
-    # Calcular métricas para cada categoria
-    metrics_per_category = {}
-    for category in categories:
-        # Filtrar apenas as linhas correspondentes à categoria atual
-        category_data = merged[merged['category_true'] == category]
-        
-        # Calcular precisão, recall e F1-Score para essa categoria
-        precision = precision_score(category_data['category_true'], category_data['category_pred'], average='weighted', zero_division=0)
-        recall = recall_score(category_data['category_true'], category_data['category_pred'], average='weighted', zero_division=0)
-        f1 = f1_score(category_data['category_true'], category_data['category_pred'], average='weighted', zero_division=0)
-        
-        metrics_per_category[category] = {
-            'precision': f"{precision:.2f}",
-            'recall': f"{recall:.2f}",
-            'f1_score': f"{f1:.2f}"
-        }
-
-    return metrics_per_category
-
-def metrics_per_label(test, input) -> dict:
-    metrics_per_label = {}
-    for column in test:
-        precision = precision_score(test[column], input[column], average='weighted', zero_division=0)
-        recall = recall_score(test[column], input[column], average='weighted', zero_division=0)
-        f1 = f1_score(test[column], input[column], average='weighted', zero_division=0)
-        
-        metrics_per_label[column] = {
-            'precision': f"{precision:.2f}",
-            'recall': f"{recall:.2f}",
-            'f1_score': f"{f1:.2f}"
-        }
-        
-    return metrics_per_label
-
-def metrics_per_label_with_fuzzy(test, input, threshold=70) -> dict:
-    metrics_per_label = {}
-
-    for column in test:
-        true_labels = []
-        pred_labels = []
-
-        for t, i in zip(test[column], input[column]):
-            # Calcular a similaridade fuzzy
-            similarity = fuzz.token_sort_ratio(str(t), str(i))
-            
-            # Verificar se a similaridade está acima do limiar
-            if similarity >= threshold:
-                true_labels.append(1)  # Match correto
-                pred_labels.append(1)
-            else:
-                true_labels.append(1)  # Sempre conta como positivo no teste
-                pred_labels.append(0)  # Match incorreto
-        
-        # Calcular precisão, recall e F1-Score para a coluna
-        precision = precision_score(true_labels, pred_labels, zero_division=0)
-        recall = recall_score(true_labels, pred_labels, zero_division=0)
-        f1 = f1_score(true_labels, pred_labels, zero_division=0)
-        
-        metrics_per_label[column] = {
-            'precision': f"{precision:.2f}",
-            'recall': f"{recall:.2f}",
-            'f1_score': f"{f1:.2f}"
-        }
-
-    return metrics_per_label
-
-
-# def accuracy_by_category(test, output):
-#     # Combinar os DataFrames test e output para comparação
-#     merged = test[['category', 'action']].merge(output[['category', 'action']], 
-#                                                 left_index=True, 
-#                                                 right_index=True, 
-#                                                 suffixes=('_true', '_pred'))
-
-#     # Filtrar categorias únicas
-#     categories = merged['category_true'].unique()
-
-#     # Calcular acurácia para cada categoria
-#     accuracy_per_category = {}
-#     for category in categories:
-#         # Filtrar apenas as linhas correspondentes à categoria atual
-#         category_data = merged[merged['category_true'] == category]
-        
-#         # Calcular a acurácia para essa categoria
-#         accuracy = accuracy_score(category_data['category_true'], category_data['category_pred'])
-#         accuracy_per_category[category] = accuracy
-
-#     return accuracy_per_category
-
-
-
-# def accuracy_per_label_with_fuzzy(test, input, threshold=70) -> dict:
-#     accuracy_per_label = {}
-    
-#     for column in test:
-#         matches = []
-#         for t, i in zip(test[column], input[column]):
-#             # Calcular a similaridade fuzzy
-#             similarity = fuzz.token_sort_ratio(str(t), str(i))
-            
-#             # Verificar se a similaridade está acima do limiar
-#             if similarity >= threshold:
-#                 matches.append(1)  # Conta como correto
-#             else:
-#                 matches.append(0)  # Conta como incorreto
-        
-#         # Calcular a acurácia considerando os matches fuzzy
-#         accuracy_per_label[column] = sum(matches) / len(matches)
-    
-#     return accuracy_per_label
-
-# def accuracy_per_label(test, input) -> dict:
-#     accuracy_per_label = {}
-#     for column in test:
-#         accuracy_per_label[column] = accuracy_score(test[column], input[column])
-        
-#     return accuracy_per_label
-
-
-
 
 def save_results(file, test_size, acc_class, acc_category, acc_fuzzy):
     directory = os.path.dirname(file)
@@ -208,29 +32,96 @@ def get_test_set(test_path):
     return test_set
 
 
+def newReq(input_df, model):
+    columns = ["intent", "category", "action", "requirement",
+                "targets", "magnitude", "start_time", "end_time"]
+    output_data = []
+    llm = OllamaLLM(model=model)
+
+    for _, row in tqdm(input_df.iterrows(), desc="Obtendo outputs"):
+        intent = row['intent']
+        response = llm.invoke(intent)
+
+        if response:
+            response = response.strip()
+            try:
+                response_json = json.loads(response)
+                output_data.append(response_json)
+            except json.JSONDecodeError as e:
+                output_data.append({col: "" for col in columns})
+                output_data[-1]['category'] = "error"
+        else:
+            output_data.append({col: "" for col in columns})
+            output_data[-1]['category'] = "error"
+
+    output_df = pd.DataFrame(output_data)
+    output_df = output_df.fillna("").astype(str).applymap(lambda x: x.lower())
+    output_df = output_df.drop(columns=['start_time', 'end_time'])
+
+    with open('eval-data/output_recente.json', 'w') as f:
+        json.dump(output_data, f, indent=4)
+
+    return output_df
 
 
 
 
-    
 
-def evaluation_llms(test_file_path, test_size, modelName, quantized = None):
+def calcularResultados(tp, fp, fn):
+    try:
+        precision = (tp) / (tp + fp)
+    except ZeroDivisionError:
+        precision = 0
+    try:
+        recall = (tp) / (tp + fn)
+    except ZeroDivisionError:
+        recall = 0
+    try:
+        f1 = 2*((precision * recall) / (precision + recall))
+    except ZeroDivisionError:
+        f1 = 0
+    return precision, recall, f1
+
+
+def calculateMetricsCategory(pred_categorias, test_categorias):
+    unique_categories = set(pred_categorias).union(set(test_categorias))
+    results = []
+    for category in unique_categories:
+        # Condições para cada métrica
+        tp = float(((pred_categorias == category) & (test_categorias == category)).sum())
+        fp = float(((pred_categorias == category) & (test_categorias != category)).sum())
+        fn = float(((pred_categorias != category) & (test_categorias == category)).sum())
+        precision, recall, f1 = calcularResultados(tp, fp, fn)        
+        new_json = {
+            'category': category,
+            'precision': f"{precision:.2f}",
+            'recall': f"{recall:.2f}",
+            'f1-score': f"{f1:.2f}",
+        }
+        results.append(new_json)
+
+    return results
+
+
+def calculateMetricsAction(test_set, output_set):
+    pass
+
+def evaluation_llms(test_file_path, test_size, modelName):
+    # Usar os arquivos salvos para testar
+    # output_set = pd.read_json("eval-data/output_recente.json")
+    # output_set = output_set.fillna("").astype(str).applymap(lambda x: x.lower())
+    # output_set = output_set.drop(columns=['start_time', 'end_time'])
+    # test_set = pd.read_json("./eval-data/test_recente.json")
+    test_size = 15
     test_set = get_test_set(test_file_path)
     intents = pd.DataFrame(test_set['intent'])
-    save_test_shuffled(test_set)
-    new_test_set = test_set.copy()
+    save_test_shuffled(test_set.head(test_size))
     # Para podermos verificar como o modelo se comporta conforme aumentamos os
-    output, new_test_set = req(intents.head(test_size), new_test_set.head(test_size), modelName)
-    
-    # Calcula as acurácias
-    metrics_label = metrics_per_label(new_test_set, output)
-    metrics_cat = metrics_by_category(new_test_set, output)
-    # Avaliar com fuzzy
-    # Categoria e action bater 100%, logo são removido da aproximação com fuzzy
-    test_fuzzy = new_test_set.drop(columns=['category', 'action'])
-    out_fuzzy = output.drop(columns=['category', 'action'])
-    metrics_label_fuzzy = metrics_per_label_with_fuzzy(test_fuzzy, out_fuzzy)
-    if (quantized): save_results(f"./results/{modelName}-{quantized}b/accuracy.txt", test_size, metrics_label, metrics_cat, metrics_label_fuzzy)
-    else: save_results(f"./results/{modelName}/accuracy.txt", test_size, metrics_label, metrics_cat, metrics_label_fuzzy)
+    output_set = newReq(intents.head(test_size), modelName)
+    metrics_cat = calculateMetricsCategory(output_set['category'], test_set['category'])
+    print(metrics_cat)
+
+
+
 
 
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from eval import evaluation_llms
 
 if __name__ == "__main__":
     test_file_path = "eval-data/test_500.json"
-    test_set_size = 10
+    test_set_size = 5
     # Se quiser testar varios modelos de uma só vez, bota o modelo na lista da forma de tuple (nome_do_modelo, quantização) -> None para não quantizado 
-    all_models = [("customLlama:latest", None)]
-    for modelName, quantization in all_models:
-        evaluation_llms(test_file_path=test_file_path, test_size=test_set_size, modelName=modelName, quantized=quantization)
+    all_models = ["agir-llama3:8b"]
+    for modelName in all_models:
+        evaluation_llms(test_file_path=test_file_path, test_size=test_set_size, modelName=modelName)
     

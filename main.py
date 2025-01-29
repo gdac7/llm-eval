ddd772cf9b011eac02fb2881fb7916d4b41bfe0c
gdac <gabrieldalcs@gmail.com>
2025-01-29 20:28:47 -0300

    finish results

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     test_file_path = "eval-data/test_500.json"
     test_set_size = 500
     # Nome do modelo que irá ser avaliado (Independente da quantização)
-    modelBase = ["llama", "phi"]
+    modelBase = ["qwen"]
     # Avalie em pares: Modelo não quantizado vs Modelo quantizado.
-    modelVersions = ["llama3.1-q4km:latest", "phi4-q4km:latest"]
+    modelVersions = ["qwen2.5-fp16:latest"]
     # Ex: modelBase = ["llama", "llama", "phi"]
     #     modelVersions = ['llama3:8b', 'llama3:16b', 'phi8b']
     # A iteração será feita em (llama, llama3:8b), (llama, llama3:16b), (phi, phi8b).

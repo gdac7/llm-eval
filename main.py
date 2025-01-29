864b63fe13ee6c27767c026669fc017ffa77b8e4
gdac <gabrieldalcs@gmail.com>
2025-01-28 21:27:27 -0300

    change main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ if __name__ == "__main__":
     test_file_path = "eval-data/test_500.json"
     test_set_size = 500
     # Nome do modelo que irá ser avaliado (Independente da quantização)
-    modelName = "llama"
+    modelBase = ["llama", "phi"]
     # Avalie em pares: Modelo não quantizado vs Modelo quantizado.
-    all_models = ["llama3:8bQ4_0"]
-    # Ex: modelName: Llama
-    # all_models = ['llama3:8b', 'llama3:16b']
-    for modelVariant in all_models:
+    modelVersions = ["llama3.1-q4km:latest", "phi4-q4km:latest"]
+    # Ex: modelBase = ["llama", "llama", "phi"]
+    #     modelVersions = ['llama3:8b', 'llama3:16b', 'phi8b']
+    # A iteração será feita em (llama, llama3:8b), (llama, llama3:16b), (phi, phi8b).
+    for modelName, modelVariant in zip(modelBase, modelVersions):
         evaluation_llms(test_file_path=test_file_path, test_size=test_set_size, model_name=modelName, current_model=modelVariant)
     
